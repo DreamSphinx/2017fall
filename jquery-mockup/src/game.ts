65d57fd4cf8bb8a9ffb90e2a52f7cbed0db5f163
Unknown <ephollowman@yahoo.com>
2017-11-02 11:01:19 -0400

    Downloaded Plotkin's code

--- jquery-mockup/src/game.ts
+++ jquery-mockup/src/game.ts
@@ -10,7 +10,16 @@ export class Player {
     score: number = 0;
 
     drawQuotes(){
-        $("#my-quotes").html(this.quotes.map(x => `<li class="list-group-item">${x.text}</li>`).join(""))
+        $("#my-quotes").html(
+            this.quotes.map( x => `<li class="list-group-item">${x.text}</li>` ).join("")
+        );
+    }
+
+    init(){
+        $.getJSON("/game/quotes").done( data =>{
+            this.quotes = data;
+            this.drawQuotes();    
+        })
     }
 }
 
@@ -23,64 +32,53 @@ export class Room {
     drawPicture() {
         $("#picture").attr("src", this.picture);
     }
-
     drawQuotes(){
-        $("#played-quotes").html(this.quotes.map(x => `<li class="list-group-item">${x.text}</li>`).join(""))
+        $("#played-quotes").html(
+            this.quotes.map( x => `<li class="list-group-item">${x.text}</li>` ).join("")
+        );
     }
-
     drawPlayers(){
-        $("#players").html(this.players.map(x => `<li class="list-group-item">${x.name}</li>`).join(""))
+        $("#players").html(
+            this.players.map( x => `<li class="list-group-item">${x.name}</li>` ).join("")
+        );
     }
 
-    update() {
-        $.get("/game/room/picture").done(data => {
+    update(){
+        $.get("/game/room/picture").done( data => {
             this.picture = data;
             this.drawPicture();
         });
-        $.getJSON("/game/room/quotes").done(data => {
+        $.getJSON("/game/room/quotes").done( data =>{
             this.quotes = data;
             this.drawQuotes();
         });
     }
 
-    init() {
-        setInterval(() => this.update(), 1000)
-    }
-}
-
-export class Game {
-    players: Player[] = [];
-    pictures: string[] = [];
-    quotes: Quote[] = [];
-
     init(){
-        return $.when(
-            $.getJSON("/game/pictures").done(data => {
-                this.pictures = data;
-            }),
-            $.getJSON("/game/quotes").done(data => {
-                this.quotes = data;
-            })
-        );
+        setInterval(()=> this.update(), 1000)
     }
 }
+
 
 // Controller
 
-const game = new Game();
 const room = new Room();
 const me = new Player();
+me.init();
 
 room.init();
+room.drawPlayers();
 
-game.init().done(() => {
-    room.drawPlayers();
-    
-    me.quotes = game.quotes;
-    me.drawQuotes();
-});
 
 $("#cmd-flip").click(function(e){
     e.preventDefault();
     $.post("/game/room/picture")
+})
+
+$("#my-quotes").click("li", function(e){
+    e.preventDefault();
+    const $li = $(e.originalEvent.srcElement);
+    const data = { text: $li.text() };
+    $.post("/game/room/quotes", data);
+    $li.remove();
 })
