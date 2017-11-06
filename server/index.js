ed5ea754e09e8a39d21c250a04d6103c86d9c073
Unknown <ephollowman@yahoo.com>
2017-11-06 11:15:21 -0500

    Added Angular

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const handler = require("./httpHandler.js")
+const handler = require("./httpHandler");
 const gameController = require("./gameController");
+const bodyParser = require("body-parser");
 
 const server = express();
 
+server.use(bodyParser.urlencoded());
 server.use("/client", express.static("./jquery-mockup"))
 server.use("/old", handler.main);
-server.use("/game", gameController.router);
+server.use("/game", gameController.router );
+    
 
+server.listen(3001);
 
-server.listen(3000);
-
-console.log("http://localhost:3000");
+console.log("http://localhost:3001");
